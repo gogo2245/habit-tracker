75591488bd0bc8a6468c2aeaab5567e220edd66d
gogo2245 <adrian.gogolak@gmail.com>
2022-04-24 22:53:01 +0200

    Add complete habit logic

--- habsheet/src/database/habits.ts
+++ habsheet/src/database/habits.ts
@@ -1,6 +1,6 @@
 import {DynamoDB} from 'aws-sdk'
 import * as _ from 'lodash'
-import {DatabaseHabit} from 'src/types/database'
+import {DatabaseHabit, HabitInterval} from 'src/types/database'
 import * as uuid from 'uuid'
 import {listGroupsByUserID} from './groups'
 
@@ -34,3 +34,28 @@ export const listAllHabits = async (userID: string): Promise<DatabaseHabit[]> =>
   const habits = await Promise.all(_.map(groups, ({id}) => listHabits(id)))
   return _.flatten(habits)
 }
+
+export const getHabit = async (habitID: string, groupID: string): Promise<DatabaseHabit | undefined> => {
+  return (await ddb.get({TableName: process.env.HabitsTableName, Key: {id: habitID, groupID}}).promise())
+    .Item as DatabaseHabit
+}
+
+export const completeHabit = async (habitID: string, groupID: string, userID: string, value: number): Promise<void> => {
+  const habit = await getHabit(habitID, groupID)
+  const lastDate = new Date()
+  switch (habit.interval) {
+    case HabitInterval.daily:
+      break
+    case HabitInterval.weekly:
+      lastDate.setDate(lastDate.getDate() - ((lastDate.getDay() + 6) % 7))
+      break
+    case HabitInterval.monthly:
+      lastDate.setDate(1)
+      break
+  }
+  const date = new Date(lastDate.toISOString().slice(0, 10)).getTime()
+  await ddb.put({TableName: process.env.HabitInsancesTableName, Item: {date, habitID, userID, value}}).promise()
+}
+
+export const isHabit = async (habitID: string, groupID: string): Promise<boolean> =>
+  !!(await getHabit(habitID, groupID))
